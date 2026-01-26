a9cfc6538fa15d09991d81dac37a14719aa197ad
ibrokhimovnba <ibrokhimovnba@gmail.com>
2026-01-26 22:33:11 +0500

    RoomCraft

--- utils/document.js
+++ utils/document.js
@@ -1,4 +1,35 @@
-  document.querySelector('.index__previous__btn').addEventListener('click', () => {
+  "use strict";
+  
+    Toastify({
+    text: "RoomCraft mebel do'koniga xush kelibsiz !",
+    duration: 3500,
+    gravity: "top",
+    position: "center",
+    close: true,
+    style: {
+      background: "linear-gradient(to right, black, black)",
+      color: "red",
+      border: "1px solid red",
+      boxShadow: "0 0 12px rgba(255, 0, 0, 0.6)"
+    }
+  }).showToast();
+
+  Toastify({
+    text: "Assalomu alaykum",
+    duration: 3500,
+    gravity: "top",
+    position: "center",
+    close: true,
+    style: {
+      background: "linear-gradient(to right, black, black)",
+      color: "green",
+      border: "1px solid green",
+      boxShadow: "0 0 12px rgba(0, 255, 0, 0.6)"
+    }
+  }).showToast();
+  
+  
+document.querySelector('.index__previous__btn').addEventListener('click', () => {
   window.location.href = '/index.html'; 
 });
 
